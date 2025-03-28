edce110872aa82062c372891413bd66a6e633cd2
Kristopher Lee <6152682+kalee1@users.noreply.github.com>
2025-03-28 12:28:46 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Pixycam/OpMode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Robot.Pixycam;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(group = "Teleop")
+
+public class OpMode extends LinearOpMode {
+
+    private Pixy pixy;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        pixy = hardwareMap.get(Pixy.class, "deviceName");
+        PixyBlock detectedBlock;
+
+        pixy.turnOffLamps();
+
+        waitForStart();
+        while(opModeIsActive()){
+
+            detectedBlock = pixy.getBlock();
+
+            telemetry.addLine(detectedBlock.toString());
+
+            if(detectedBlock.isValid()){
+                // do something
+            }
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Pixycam/PixyBlock.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Robot.Pixycam;
+
+
+public class PixyBlock {
+
+    public int checksum;
+    public int signature;
+    public int centerX;
+    public int centerY;
+    public int width;
+    public int height;
+    public int angle;
+    public int trackingIndex;
+    public int age;
+
+    public boolean isValid(){
+        return true;
+    }
+
+    @Override
+    public String toString(){
+        return "[ signature: " + signature + "\ncenterX: " + centerX + "\ncenterY: " + centerY + "\nwidth: " + width + "\nheight: " + height + "\nage: " + age + " ]";
+    }
+}
