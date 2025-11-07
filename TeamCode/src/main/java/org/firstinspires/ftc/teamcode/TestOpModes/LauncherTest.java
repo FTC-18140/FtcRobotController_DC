095c2bb1891852847b8248d46ecf13bfc410e32d
River <jeremy.gbsn@gmail.com>
2025-11-07 17:39:42 -0600

    Slight algorithm adjustment for tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/LauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/LauncherTest.java
@@ -1,29 +1,34 @@
 package org.firstinspires.ftc.teamcode.TestOpModes;
 
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.Robot.ThunderBot2025;
 import org.firstinspires.ftc.teamcode.Utilities.TBDGamepad;
 
 @TeleOp
 public class LauncherTest extends OpMode {
     private TBDGamepad theGamepad1;
-    DcMotor launcher = null;
+    ThunderBot2025 robot = null;
 
     @Override
     public void init() {
-        launcher = hardwareMap.dcMotor.get("launcher");
+        robot.init(hardwareMap, telemetry, new Pose2d(0,0, 0));
 
         theGamepad1 = new TBDGamepad(gamepad1);
     }
 
     @Override
     public void loop() {
+        theGamepad1.update();
         if(theGamepad1.getButton(TBDGamepad.Button.X)){
-            launcher.setPower(1.0);
         } else if (theGamepad1.getButton(TBDGamepad.Button.B)) {
-            launcher.setPower(0.0);
+        }
+
+        if(theGamepad1.getButtonPressed(TBDGamepad.Button.RIGHT_BUMPER)){
+            robot.lockOn();
         }
     }
 }
