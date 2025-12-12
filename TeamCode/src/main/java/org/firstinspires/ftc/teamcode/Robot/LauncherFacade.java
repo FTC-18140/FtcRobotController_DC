a432b45683cb2d3de959fa24066c00506cc2c455
TINYTECH\oneil <oneillnate.d@gmail.com>
2025-12-12 09:20:09 -0600

    Merge fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/LauncherFacade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/LauncherFacade.java
@@ -135,9 +135,9 @@ public class LauncherFacade implements DataLoggable {
         return turret.getCurrentPosition() + difference;
 
     }
-    public void setOffestAngle(double angle) {
-        turret.setOffsetAngle(angle);
-    }
+//    public void setOffestAngle(double angle) {
+//        turret.setOffsetAngle(angle);
+//    }
 
     /** Prepares the flywheel for a shot based on the robot's current pose. */
     public void prepShot() {
