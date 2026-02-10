8a630d503a028261485075dcd083be08b66e476d
River <jeremy.gbsn@gmail.com>
2026-02-10 09:38:18 -0600

    Prep for 2nd Beam Break sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/BeamBreaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/BeamBreaker.java
@@ -10,21 +10,22 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class BeamBreaker {
     private Telemetry telemetry;
     private boolean detected = false;
-    DigitalChannel beamBreakSensor = null;
+    DigitalChannel beamBreakIndexer = null;
+    DigitalChannel beamBreakIntake = null;
     public static boolean TELEM = true;
 
     public void init(HardwareMap hwMap, Telemetry telem) {
         telemetry = telem;
         try {
-            beamBreakSensor = hwMap.digitalChannel.get("beamBreak");
+            beamBreakIndexer = hwMap.digitalChannel.get("beamBreak");
         } catch (Exception e) {
             telemetry.addData("Error", "Could not find digital channel 'beamBreak'");
         }
     }
 
     public void update() {
-        if(beamBreakSensor != null) {
-            detected = !beamBreakSensor.getState();
+        if(beamBreakIndexer != null) {
+            detected = !beamBreakIndexer.getState();
             if (TELEM) {
                 telemetry.addData("Beam Break sensor triggered: ", detected);
             }
