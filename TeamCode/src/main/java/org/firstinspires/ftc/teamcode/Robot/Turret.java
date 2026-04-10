d3c32a0c0ba6e6e09d3df7ddd0078f83937de0de
Nathan <oneillnate.d@gmail.com>
2026-04-10 17:26:34 -0500

    Turret tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Turret.java
@@ -44,20 +44,18 @@ public class Turret implements DataLoggable {
     private Telemetry telemetry;
 
     // Tunable constants from your original file
-    public static double P_TURRET = 0.024, I_TURRET = 0.022, D_TURRET = 0.001, F_TURRET_MIN = 0.0, F_TURRET_MAX = 0.018;
+    public static double P_TURRET = 0.015, I_TURRET = 0.029, D_TURRET = 0.0012, F_TURRET_MIN = 0.0, F_TURRET_MAX = 0.018;
     public static double MAX_TURRET_POS = 270.0;
     public static double MIN_TURRET_POS = -90.0;
     public static double TURRET_ANGLE_TOLERANCE = 2.5;
 
-    public static double KV_ROT = 0.17; // Tunable: Gain for robot rotation
+    public static double KV_ROT = 0.21; // Tunable: Gain for robot rotation
     public static double KV_TRANS = 0.17; // Tunable: Gain for translational apparent rotation
     public static boolean TELEM = false;
 
-    public static double MAX_POWER = 0.55;
+    public static double MAX_POWER = 0.6;
     public static double MIN_POWER_POSITIVE = 0.015;
     public static double MIN_POWER_NEGATIVE = -0.015;
-
-    public static double TURN_SPEED = 208.3; // From original lockOn
     public static double TURRET_DEGREES_PER_ENCODER_TICK = (double) 1 / 8192.0 * 360.0 * 16 / 100;
 
 
