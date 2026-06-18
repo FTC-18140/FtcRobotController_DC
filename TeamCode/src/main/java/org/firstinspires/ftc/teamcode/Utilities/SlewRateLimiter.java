ba875f0aece1346b9da7b7bc854e3480a9eaaf3f
Kris Lee <6152682+kalee1@users.noreply.github.com>
2026-06-17 22:24:31 -0500

    Create SlewRateLimiter.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/SlewRateLimiter.java
@@ -0,0 +1,26 @@
+public class SlewRateLimiter
+{
+    private double value = 0.0;
+    private double maxDelta;
+
+    public SlewRateLimiter(double maxDelta)
+    {
+        this.maxDelta = maxDelta;
+    }
+
+    public double update(double target)
+    {
+        double delta = target - value;
+
+        if (Math.abs(delta) > maxDelta)
+        {
+            value += Math.signum(delta) * maxDelta;
+        }
+        else
+        {
+            value = target;
+        }
+
+        return value;
+    }
+}
